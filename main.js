15b1a9f63051133a536d2ac5c2ba2d114e49f530
Natan Bond <97616282+NatanBondarenko@users.noreply.github.com>
2023-06-25 12:39:06 +0300

    Update main.js

--- main.js
+++ main.js
@@ -18,19 +18,40 @@ looker.plugins.visualizations.add({
   },
 
   create: function(element, config) {
-    element.innerHTML = config.html_code;
+    element.innerHTML = '';
   },
 
   update: function(data, element, config, queryResponse) {
+    const htmlCode = config.html_code;
     const jsCode1 = config.js_code_1;
     const jsCode2 = config.js_code_2;
 
-    // Execute JavaScript code 1
-    eval(jsCode1);
+    // Create a new document object
+    const doc = new DOMParser().parseFromString(htmlCode, 'text/html');
+
+    // Retrieve the body element from the new document
+    const bodyElement = doc.querySelector('body');
+
+    // Clear the element and append the body content
+    const container = element.firstChild;
+    container.innerHTML = '';
+    container.appendChild(bodyElement);
+
+    // Create a new script element for JavaScript code 1
+    const script1 = document.createElement('script');
+    script1.textContent = jsCode1;
+
+    // Append the script element to the body
+    bodyElement.appendChild(script1);
 
     // Execute JavaScript code 2 if provided
     if (jsCode2) {
-      eval(jsCode2);
+      // Create a new script element for JavaScript code 2
+      const script2 = document.createElement('script');
+      script2.textContent = jsCode2;
+
+      // Append the script element to the body
+      bodyElement.appendChild(script2);
     }
   }
 });
