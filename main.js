93a3b95b994e8eb719d9108fd2529e053368d991
Natan Bond <97616282+NatanBondarenko@users.noreply.github.com>
2023-06-25 12:52:45 +0300

    Update main.js

--- main.js
+++ main.js
@@ -3,43 +3,47 @@ looker.plugins.visualizations.add({
     html_code: {
       type: 'string',
       label: 'HTML Code',
-      default: '<html>\n<head>\n<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>\n</head>\n<body>\n  <div id="piechart" style="width: 900px; height: 500px;"></div>\n</body>\n</html>',
+      default: '<div>Hello, World!</div>',
     },
-    js_code: {
+    css_code: {
       type: 'string',
-      label: 'JavaScript Code',
-      default: 'google.charts.load(\'current\', {\'packages\':[\'corechart\']});\ngoogle.charts.setOnLoadCallback(drawChart);\n\nfunction drawChart() {\n  var data = google.visualization.arrayToDataTable([\n    [\'Task\', \'Hours per Day\'],\n    [\'Work\',     11],\n    [\'Eat\',      2],\n    [\'Commute\',  2],\n    [\'Watch TV\', 2],\n    [\'Sleep\',    7]\n  ]);\n\n  var options = {\n    title: \'My Daily Activities\'\n  };\n\n  var chart = new google.visualization.PieChart(document.getElementById(\'piechart\'));\n\n  chart.draw(data, options);\n}',
+      label: 'CSS Code',
+      default: 'div { color: red; }',
+    },
+    js_code1: {
+      type: 'string',
+      label: 'JavaScript Code 1',
+      default: '<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>',
+    },
+    js_code2: {
+      type: 'string',
+      label: 'JavaScript Code 2',
+      default: '<script type="text/javascript">google.charts.load(\'current\', {\'packages\':[\'corechart\']}); google.charts.setOnLoadCallback(drawChart); function drawChart() { var data = google.visualization.arrayToDataTable([ [\'Task\', \'Hours per Day\'], [\'Work\', 11], [\'Eat\', 2], [\'Commute\', 2], [\'Watch TV\', 2], [\'Sleep\', 7] ]); var options = { title: \'My Daily Activities\' }; var chart = new google.visualization.PieChart(document.getElementById(\'piechart\')); chart.draw(data, options); }</script>',
     },
   },
 
   create: function(element, config) {
-    element.innerHTML = '';
+    element.innerHTML = '<div id="visualization-container"></div>';
   },
 
   update: function(data, element, config, queryResponse) {
+    const container = element.querySelector('#visualization-container');
     const htmlCode = config.html_code;
-    const jsCode = config.js_code;
+    const cssCode = config.css_code;
+    const jsCode1 = config.js_code1;
+    const jsCode2 = config.js_code2;
 
-    // Create a new iframe element
-    const iframe = document.createElement('iframe');
-    iframe.style.width = '100%';
-    iframe.style.height = '100%';
+    container.innerHTML = htmlCode;
+    const styleElement = document.createElement('style');
+    styleElement.textContent = cssCode;
+    container.appendChild(styleElement);
 
-    // Append the iframe to the element
-    element.innerHTML = '';
-    element.appendChild(iframe);
+    const jsElement1 = document.createElement('script');
+    jsElement1.innerHTML = jsCode1;
+    container.appendChild(jsElement1);
 
-    // Create a new document object inside the iframe
-    const doc = iframe.contentWindow.document;
-
-    // Write the HTML code into the document
-    doc.open();
-    doc.write(htmlCode);
-    doc.close();
-
-    // Append JavaScript code as a script element to the document
-    const script = doc.createElement('script');
-    script.textContent = jsCode;
-    doc.head.appendChild(script);
+    const jsElement2 = document.createElement('script');
+    jsElement2.innerHTML = jsCode2;
+    container.appendChild(jsElement2);
   }
 });
