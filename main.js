99cc3f7a89a87b8755d16d860537696281952f1c
Natan Bond <97616282+NatanBondarenko@users.noreply.github.com>
2023-06-25 12:43:40 +0300

    Update main.js

--- main.js
+++ main.js
@@ -3,17 +3,17 @@ looker.plugins.visualizations.add({
     html_code: {
       type: 'string',
       label: 'HTML Code',
-      default: '<html>\n<head>\n\n</head>\n<body>\n  <div id="piechart" style="width: 900px; height: 500px;"></div>\n</body>\n</html>',
+      default: '<html>\n<head>\n</head>\n<body>\n  <div id="piechart" style="width: 900px; height: 500px;"></div>\n</body>\n</html>',
     },
     js_code_1: {
       type: 'string',
       label: 'JavaScript Code 1',
-      default: '<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>',
+      default: 'https://www.gstatic.com/charts/loader.js',
     },
     js_code_2: {
       type: 'string',
       label: 'JavaScript Code 2',
-      default: '<script type="text/javascript">\n  google.charts.load(\'current\', {\'packages\':[\'corechart\']});\n  google.charts.setOnLoadCallback(drawChart);\n\n  function drawChart() {\n\n    var data = google.visualization.arrayToDataTable([\n      [\'Task\', \'Hours per Day\'],\n      [\'Work\',     11],\n      [\'Eat\',      2],\n      [\'Commute\',  2],\n      [\'Watch TV\', 2],\n      [\'Sleep\',    7]\n    ]);\n\n    var options = {\n      title: \'My Daily Activities\'\n    };\n\n    var chart = new google.visualization.PieChart(document.getElementById(\'piechart\'));\n\n    chart.draw(data, options);\n  }\n</script>',
+      default: 'google.charts.load(\'current\', {\'packages\':[\'corechart\']});\ngoogle.charts.setOnLoadCallback(drawChart);\n\nfunction drawChart() {\n  var data = google.visualization.arrayToDataTable([\n    [\'Task\', \'Hours per Day\'],\n    [\'Work\',     11],\n    [\'Eat\',      2],\n    [\'Commute\',  2],\n    [\'Watch TV\', 2],\n    [\'Sleep\',    7]\n  ]);\n\n  var options = {\n    title: \'My Daily Activities\'\n  };\n\n  var chart = new google.visualization.PieChart(document.getElementById(\'piechart\'));\n\n  chart.draw(data, options);\n}',
     },
   },
 
@@ -26,31 +26,31 @@ looker.plugins.visualizations.add({
     const jsCode1 = config.js_code_1;
     const jsCode2 = config.js_code_2;
 
-    // Create a new document object
-    const doc = new DOMParser().parseFromString(htmlCode, 'text/html');
+    // Create a new iframe element
+    const iframe = document.createElement('iframe');
+    iframe.style.width = '100%';
+    iframe.style.height = '100%';
 
-    // Retrieve the head and body elements from the new document
-    const headElement = doc.querySelector('head');
-    const bodyElement = doc.querySelector('body');
+    // Append the iframe to the element
+    element.innerHTML = '';
+    element.appendChild(iframe);
 
-    // Clear the element and append the head and body content
-    const container = element.firstChild;
-    container.innerHTML = '';
-    container.appendChild(headElement);
-    container.appendChild(bodyElement);
+    // Create a new document object inside the iframe
+    const doc = iframe.contentWindow.document;
 
-    // Create a new script element for JavaScript code 1
-    const script1 = document.createElement('script');
-    script1.innerHTML = jsCode1;
+    // Write the HTML code into the document
+    doc.open();
+    doc.write(htmlCode);
+    doc.close();
 
-    // Append the script element to the head
-    headElement.appendChild(script1);
+    // Append JavaScript code 1 as a script element to the document
+    const script1 = doc.createElement('script');
+    script1.src = jsCode1;
+    doc.head.appendChild(script1);
 
-    // Create a new script element for JavaScript code 2
-    const script2 = document.createElement('script');
-    script2.innerHTML = jsCode2;
-
-    // Append the script element to the body
-    bodyElement.appendChild(script2);
+    // Append JavaScript code 2 as a script element to the document
+    const script2 = doc.createElement('script');
+    script2.textContent = jsCode2;
+    doc.head.appendChild(script2);
   }
 });
