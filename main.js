7d91b0ab9574976359e8fddbe464b334af890706
Natan Bond <97616282+NatanBondarenko@users.noreply.github.com>
2023-06-25 12:41:44 +0300

    Update main.js

--- main.js
+++ main.js
@@ -3,17 +3,17 @@ looker.plugins.visualizations.add({
     html_code: {
       type: 'string',
       label: 'HTML Code',
-      default: '<html>\n<head>\n\n</head>\n<body style="font-family: Arial;border: 0 none;">\n  <div id="vis_div" style="width: 600px; height: 400px;"></div>\n</body>\n</html>',
+      default: '<html>\n<head>\n\n</head>\n<body>\n  <div id="piechart" style="width: 900px; height: 500px;"></div>\n</body>\n</html>',
     },
     js_code_1: {
       type: 'string',
       label: 'JavaScript Code 1',
-      default: 'google.charts.load(\'current\');\ngoogle.charts.setOnLoadCallback(drawVisualization);\n\nfunction drawVisualization() {\n  var wrapper = new google.visualization.ChartWrapper({\n    chartType: \'LineChart\',\n    dataSourceUrl: \'http://spreadsheets.google.com/tq?key=pCQbetd-CptGXxxQIG7VFIQ&pub=1\',\n    query: \'SELECT A,D WHERE D > 100 ORDER BY D\',\n    options: {\'title\': \'Countries\'},\n    containerId: \'vis_div\'\n  });\n  wrapper.draw()\n\n  // No query callback handler needed!\n}',
+      default: '<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>',
     },
     js_code_2: {
       type: 'string',
       label: 'JavaScript Code 2',
-      default: '',
+      default: '<script type="text/javascript">\n  google.charts.load(\'current\', {\'packages\':[\'corechart\']});\n  google.charts.setOnLoadCallback(drawChart);\n\n  function drawChart() {\n\n    var data = google.visualization.arrayToDataTable([\n      [\'Task\', \'Hours per Day\'],\n      [\'Work\',     11],\n      [\'Eat\',      2],\n      [\'Commute\',  2],\n      [\'Watch TV\', 2],\n      [\'Sleep\',    7]\n    ]);\n\n    var options = {\n      title: \'My Daily Activities\'\n    };\n\n    var chart = new google.visualization.PieChart(document.getElementById(\'piechart\'));\n\n    chart.draw(data, options);\n  }\n</script>',
     },
   },
 
@@ -29,29 +29,28 @@ looker.plugins.visualizations.add({
     // Create a new document object
     const doc = new DOMParser().parseFromString(htmlCode, 'text/html');
 
-    // Retrieve the body element from the new document
+    // Retrieve the head and body elements from the new document
+    const headElement = doc.querySelector('head');
     const bodyElement = doc.querySelector('body');
 
-    // Clear the element and append the body content
+    // Clear the element and append the head and body content
     const container = element.firstChild;
     container.innerHTML = '';
+    container.appendChild(headElement);
     container.appendChild(bodyElement);
 
     // Create a new script element for JavaScript code 1
     const script1 = document.createElement('script');
-    script1.textContent = jsCode1;
+    script1.innerHTML = jsCode1;
+
+    // Append the script element to the head
+    headElement.appendChild(script1);
+
+    // Create a new script element for JavaScript code 2
+    const script2 = document.createElement('script');
+    script2.innerHTML = jsCode2;
 
     // Append the script element to the body
-    bodyElement.appendChild(script1);
-
-    // Execute JavaScript code 2 if provided
-    if (jsCode2) {
-      // Create a new script element for JavaScript code 2
-      const script2 = document.createElement('script');
-      script2.textContent = jsCode2;
-
-      // Append the script element to the body
-      bodyElement.appendChild(script2);
-    }
+    bodyElement.appendChild(script2);
   }
 });
